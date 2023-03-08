8e05b33257dbfda044005233159bab6f543602ed
pechtelt1 <patrick@van-echtelt.nl>
2023-03-08 10:51:36 +0100

    new

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,14 +5,14 @@
                     class="text-indigo-600" data-primary="indigo-600">.</span>nl</span>
     </a>
 
-        <div class="inline-flex items-center space-x-6 md:w-2/5 md:justify-end">
-            <a href="#donate"
-               class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"><span
-                    style="font-size: 18px; font-weight: 600; text-align: center;">☕️ Doneer</span></a>
-            <a href="#uploaden"
-               class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
-               data-rounded="rounded-md" data-primary="indigo-600">Uploaden</a>
-        </div>
+{{--        <div class="inline-flex items-center space-x-6 md:w-2/5 md:justify-end">--}}
+{{--            <a href="#donate"--}}
+{{--               class="text-base font-medium leading-6 text-gray-600 whitespace-no-wrap transition duration-150 ease-in-out hover:text-gray-900"><span--}}
+{{--                    style="font-size: 18px; font-weight: 600; text-align: center;">☕️ Doneer</span></a>--}}
+{{--            <a href="#uploaden"--}}
+{{--               class="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"--}}
+{{--               data-rounded="rounded-md" data-primary="indigo-600">Uploaden</a>--}}
+{{--        </div>--}}
 
 </div>
 
