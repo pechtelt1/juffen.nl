08638155ed0dcc0797b865fe8fb9df2cae044f6b
pechtelt1 <patrick@van-echtelt.nl>
2023-03-09 14:21:38 +0100

    Update category-buttons.blade.php

--- resources/views/components/category-buttons.blade.php
+++ resources/views/components/category-buttons.blade.php
@@ -1,3 +1,5 @@
+{{--TODO: make this dynamic--}}
+
 <a href="#"
    class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
    style="font-size: 10px">Groep 7
